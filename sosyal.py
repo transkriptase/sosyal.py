0f01ae575493f322d46eba6e28f6550d823db6e5
transkriptase <139242491+transkriptase@users.noreply.github.com>
2024-11-13 17:31:31 +0100

    Update sosyal.py

--- sosyal.py
+++ sosyal.py
@@ -2,7 +2,8 @@ import h5py
 import numpy as np
 from loadh5 import load_h5_data
 from fillmissing import fill_missing
-from social_behaviors import detect_proximity_interactions
+from social_behaviors import detect_leader_follower_behavior
+from proximity import detect_proximity_interactions
 
 
 filename = "h5try/7_3_dev.h5"
@@ -12,7 +13,7 @@ filled_locations = fill_missing(locations)
 # Detect and print proximity interactions
 interactions = detect_proximity_interactions(filled_locations)
 print("Detected Interactions:")
-for termite_1, termite_2, start_frame, end_frame in interactions:
+#for termite_1, termite_2, start_frame, end_frame in interactions:
     #print(f"Termites {termite_1} and {termite_2} interacted between frames {start_frame} and {end_frame}")
 
 
